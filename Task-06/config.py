7d273702d8ef001326569c4a956dbc472e06c6e8
Ganesh S <ganeshsubhashkumar@gmail.com>
2026-01-29 17:48:04 +0530

    Changed username and password for the database config

--- Task-06/config.py
+++ Task-06/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://gsk:mohanlal1983@127.0.0.1:3306/Melofi_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://username:password@127.0.0.1:3306/Melofi_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
